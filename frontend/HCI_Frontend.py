c99367ae0754c753a95d81a429db122bea3bf571
Auwate <auwate1@gmail.com>
2024-04-08 17:56:37 -0400

    Commit: Get data functionality

--- frontend/HCI_Frontend.py
+++ frontend/HCI_Frontend.py
@@ -2,7 +2,21 @@ import streamlit as st
 import requests
 import os
 
-def send_data_to_backend(data):
+recent_queries = []
+
+def get_data_from_backend(recent_queries):
+    # Write out backend url
+    backend_url = "http://backend:80/api/recent/"
+    response = requests.get(backend_url)
+    api_data = response.json()
+
+    for data in api_data:
+        if data not in recent_queries:
+            st.sidebar.write(data)
+            recent_queries.append(data)
+
+
+def send_data_to_backend(data, recent_queries):
     # Write the path to our backend api
     backend_url = "http://backend:80/api/post/"
     # Write the payload (in this case it's our data) in json format
@@ -10,8 +24,10 @@ def send_data_to_backend(data):
     # Get the response code from the backend_url api
     response = requests.post(backend_url, json=payload)
 
-    if response.status_code == 200:
-        st.success("Request sent, please wait...")
+    if response.status_code == 201 or response.status_code == 200:
+        get_data_from_backend(recent_queries)
+
+
     else:
         st.error("Failed to send. Please retry later.")
 
@@ -24,7 +40,7 @@ with st.form("get_address"):
     submitted = st.form_submit_button("Submit")
 
     if submitted and address:
-        send_data_to_backend(address)
+        send_data_to_backend(address, recent_queries)
     elif submitted:
         st.warning("Please fill out the address field")
     else:
